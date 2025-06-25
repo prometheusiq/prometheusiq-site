4966a27ac923d99607deb2af6ef80bd995eda1c7
Seth Carroll <scarroll@clg.local>
2025-06-25 18:03:39 -0400

    Clean install and fix Vite build permissions

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter,
   Routes,
   Route
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import "./index.css";
+import './index.css';
+import App from './PrometheusIQWebsite';
+import About from './pages/About';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
-// Import your pages
-import PrometheusIQWebsite from "./PrometheusIQWebsite";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PrometheusIQWebsite />} />
+        <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/portfolio" element={<Portfolio />} />
