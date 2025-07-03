4530e69caad0aa6d19a7fbf8ad543cbf6cbeb1db
Seth Carroll <scarroll@clg.local>
2025-07-02 21:06:43 -0400

    fix: use classic JSX runtime so React is imported

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // tell Vite/Babel to use the classic JSX transform,
+      // which will leave your `import React from 'react'` in place
+      jsxRuntime: 'classic'
+    })
+  ],
   build: {
     outDir: 'dist'
   }
